f6a5d59039c6171cf35c0f6a2f5a33d597e953ea
Anthony Holt <Anthony Holt>
2016-09-26 11:23:26 -0400

    learning java

---
+++ src/_03_numbers/_01_numbers.java
@@ -0,0 +1,19 @@
+package _03_numbers;
+
+public class _01_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//byte is 8 bits short is 16, int is 32, long is 64. float is 32 and double is 64
+		//byte, short, int, long are all integer
+		
+		 byte b=65; // bytes are between (-)128-127
+		 short s =65; // shorts are between (-)2^15-((2^15)-1)
+		 int i=65; // Ints are between (-)2^31-((2^31)-1)
+		 long l =65L; // longs are between (-)2^63-((2^63)-1)
+		//float and double are floating-point or Rational numbers ergo decimal versus integer
+		 float f=65f;
+		 double d=65.55;	
+	}
+
+}
---
+++ src/_03_numbers/_02_operators.java
@@ -0,0 +1,13 @@
+package _03_numbers;
+
+public class _02_operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int classes=65;
+		int absentees= 10;
+		
+
+	}
+
+}
