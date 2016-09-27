f139630b9a86f7a837a042d18191d77252b0b408
Anthony Holt <Anthony Holt>
2016-09-27 10:08:50 -0400

    thanks to instruction from the instructor learned inputs.

--- src/_05_booleans/_02_operators.java
+++ src/_05_booleans/_02_operators.java
@@ -13,9 +13,9 @@ public class _02_operators {
 		int currBalance = 10000;
 		if (currBalance==0){
 			System.out.println("Your broke son!");
-			else if (currBalance>10000){
+			 if (currBalance>10000){
 				System.out.println("You in the money!");
-				else if (currBalance == 10000){
+					if (currBalance == 10000){
 					System.out.println("Nice account.");
 				}
 				
--- src/_06_inputoutput/_01_user_input.java
+++ src/_06_inputoutput/_01_user_input.java
@@ -1,7 +1,46 @@
 package _06_inputoutput;
 
+import java.util.Scanner;
+
 public class _01_user_input {
 	public static void main(String[]args){
+		// Scanner is a java class- used for User input
+		Scanner inputInfo = new Scanner (System.in);
+		String name;
+		String birthCity;
+		String age;
+		// Setup a simple user input
+//		System.out.println("Enter your name below: ");
+//		name = inputInfo.next();
+//		System.out.println(name);
+//		
+//		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("Enter some text: ");
+//		String str = sc.nextLine();
+//		
+//		System.out.println("Enter your City below: ");
+//		birthCity = inputInfo.next();
+//		System.out.println( birthCity);
+//		
+//		System.out.println("Enter your age below: ");
+//		age = inputInfo.next();
+//		System.out.println(age);
+//		
+		// use a cancatenation to make a sentence using these inputs
+		// .next just takes the first word, while .nextLine takes the whole line till user hits enter
+		System.out.println("Name:");
+		name = inputInfo.nextLine();
 		
+		System.out.println("Birth City:");
+		birthCity = inputInfo.nextLine();
+		
+		System.out.println("Age:");
+		age = inputInfo.nextLine();
+		
+		
+		System.out.println("Hello "+ name + ".  You were born in " + birthCity + ".  You are "+ age + " years old.");
+		inputInfo.close();
+		// .close use this to keep from allowing the program to use more and more memory.
 	}
 }
